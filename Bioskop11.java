48638c956e381e97ca8205bd0d6b166f8bf151f4
yayaluft <syanuha66@gmail.com>
2025-11-15 09:25:00 +0700

    Modifikasi percobaan 1

--- Bioskop11.java
+++ Bioskop11.java
@@ -8,6 +8,13 @@ public class Bioskop11 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][1] = "Hana";
+
+        System.out.println(penonton.length);
+        
+        for (int i=0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-"+(i+1)+": " +penonton[i].length);
+        }
         
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
