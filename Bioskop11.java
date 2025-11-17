51b9552577875eb95bdf1b06ea1bfe96afa10051
yayaluft <syanuha66@gmail.com>
2025-11-17 15:35:49 +0700

    Modifikasi no 8 Percobaan 1

--- Bioskop11.java
+++ Bioskop11.java
@@ -24,8 +24,8 @@ public class Bioskop11 {
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
         
         System.out.println("Penonton pada baris ke-3: ");
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (String i : penonton[2]) {
+            System.out.println(i);
         }
     }
 }
