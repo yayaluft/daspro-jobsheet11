7bd97c1cc39133bd0d0acffa108c6550dc098b5d
yayaluft <syanuha66@gmail.com>
2025-11-17 15:27:13 +0700

    Modifikasi no. 7 Percobaan 1

--- Bioskop11.java
+++ Bioskop11.java
@@ -1,6 +1,6 @@
 public class Bioskop11 {
     public static void main(String[] args) {
-        String [][] penonton = new String[4][2];
+        String[][] penonton = new String[4][2];
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
         penonton[1][0] = "Candra";
@@ -11,15 +11,21 @@ public class Bioskop11 {
         penonton[3][1] = "Hana";
 
         System.out.println(penonton.length);
-        
+
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris: " + penonton.length);
 
         }
+        
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }
