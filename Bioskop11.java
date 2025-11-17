7a6a2d88ed9adfedaf96dbc91b05301cc95b94b7
yayaluft <syanuha66@gmail.com>
2025-11-17 15:38:39 +0700

    Modifikasi no. 9 Percobaan 1

--- Bioskop11.java
+++ Bioskop11.java
@@ -16,16 +16,8 @@ public class Bioskop11 {
             System.out.println("Panjang baris: " + penonton.length);
 
         }
-        
-
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
-        
-        System.out.println("Penonton pada baris ke-3: ");
-        for (String i : penonton[2]) {
-            System.out.println(i);
-        }
+       for (int i =0; i < penonton.length; i++) {
+        System.out.println("Penonton pada baris ke-" + (i+1) + ": "+ String.join(", ", penonton[i]));
+       }
     }
 }
