7b61fd7107b9e5dae90e8c26e0a33460b9cb371c
yayaluft <syanuha66@gmail.com>
2025-11-19 20:24:55 +0700

    Percobaan 3

---
+++ Numbers11.java
@@ -0,0 +1,8 @@
+public class Numbers11 {
+    public static void main(String[] args) {
+        int [][] myNumbers = new int [3][];
+        myNumbers[0] = new int [5];
+        myNumbers[1] = new int [3];
+        myNumbers[2] = new int [1];
+    }
+}
