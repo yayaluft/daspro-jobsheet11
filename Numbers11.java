35810d2703e97b2a212e11cf27413623d3a8438a
yayaluft <syanuha66@gmail.com>
2025-11-19 20:26:50 +0700

    Pertanyaan no.1 Percobaan 3

--- Numbers11.java
+++ Numbers11.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
+
 public class Numbers11 {
     public static void main(String[] args) {
         int [][] myNumbers = new int [3][];
         myNumbers[0] = new int [5];
         myNumbers[1] = new int [3];
         myNumbers[2] = new int [1];
+        for (int i =0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(9myNumbers[i]));
+        }
     }
 }
