d24f2523f9b0e991677bfae912fc3bf6e127c6c2
yayaluft <syanuha66@gmail.com>
2025-11-19 21:00:13 +0700

    Modifikasi Percobaan 4

--- SIAKAD11.java
+++ SIAKAD11.java
@@ -1,30 +1,37 @@
 import java.util.Scanner;
+
 public class SIAKAD11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [][] nilai = new int [4][3];
-        int totalPersiswa =0;
 
-        for (int i= 0; i < nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i+1));
+        int totalPersiswa = 0;
+        int matkul = sc.nextInt();
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int siswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int[][] nilai = new int[matkul][siswa];
 
-            for (int j =0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+1)+ " : ");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println();
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+
+            for (int j = 0; j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + " : ");
                 nilai[i][j] = sc.nextInt();
                 totalPersiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPersiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPersiswa / 3);
         }
         System.out.println("\n=====================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah");
 
-        for (int j=0; j < 3; j++) {
+        for (int j = 0; j < matkul; j++) {
             double totalPermatkul = 0;
 
-            for (int i=0; i<4; i++) {
+            for (int i = 0; i < siswa; i++) {
                 totalPermatkul += nilai[i][j];
             }
-            System.out.println("Mata kuliah "+(j+1)+" : "+totalPermatkul/4);
+            System.out.println("Mata kuliah " + (j + 1) + " : " + totalPermatkul / 4);
         }
     }
 }
